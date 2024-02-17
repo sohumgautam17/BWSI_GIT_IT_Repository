8f2bcc9ab30e11f3d703e69d0f1cc7168c1ba8bc
Sohum Gautam <sohumgautam@icloud.com>
2024-02-16 20:06:43 -0500

    Wrote new code and Commites x2

--- BWSI_Python_course.py
+++ BWSI_Python_course.py
@@ -1,10 +1,13 @@
-
-
 list = [1, 2, 3, 4, 5]
 final = []
+def practice():
+   
 
-for i in list:
-    print(i)
+    for i in list:
+        final.append(i+1)
+    return final
 
+practice()
+print(final)
 
 
